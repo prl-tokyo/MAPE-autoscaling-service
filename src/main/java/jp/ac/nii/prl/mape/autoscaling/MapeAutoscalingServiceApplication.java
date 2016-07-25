31e817062edc87ebcc059e3573eabfcd71b092cf
Lionel Montrieux <lionel@nii.ac.jp>
2016-07-25 14:14:29 +0900

    Generated Spring Boot project

---
+++ src/main/java/jp/ac/nii/prl/mape/autoscaling/MapeAutoscalingServiceApplication.java
@@ -0,0 +1,12 @@
+package jp.ac.nii.prl.mape.autoscaling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeAutoscalingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeAutoscalingServiceApplication.class, args);
+	}
+}
