b935f99c1a41c7d45fd514f1c7ad1c3f64843969
Lionel Montrieux <lionel@nii.ac.jp>
2016-07-25 15:54:09 +0900

    Instance DTO

--- src/main/java/jp/ac/nii/prl/mape/autoscaling/model/dto/InstanceDTO.java
+++ src/main/java/jp/ac/nii/prl/mape/autoscaling/model/dto/InstanceDTO.java
@@ -1,5 +1,42 @@
 package jp.ac.nii.prl.mape.autoscaling.model.dto;
 
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class InstanceDTO {
+	
+	@NotEmpty
+	private String instID;
+	
+	@NotEmpty
+	private String instType;
+	
+	@NotNull
+	private Double instLoad;
+
+	public String getInstID() {
+		return instID;
+	}
+
+	public Double getInstLoad() {
+		return instLoad;
+	}
+
+	public String getInstType() {
+		return instType;
+	}
+
+	public void setInstID(String instID) {
+		this.instID = instID;
+	}
+
+	public void setInstLoad(Double instLoad) {
+		this.instLoad = instLoad;
+	}
+
+	public void setInstType(String instType) {
+		this.instType = instType;
+	}
 
 }
