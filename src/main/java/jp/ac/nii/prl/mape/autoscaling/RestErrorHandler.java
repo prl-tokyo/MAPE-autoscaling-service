6ec80864c6f8d585a4d697bb02e2670e3e832d0b
Lionel Montrieux <lionel@nii.ac.jp>
2016-07-29 12:51:39 +0900

    REST error handler

---
+++ src/main/java/jp/ac/nii/prl/mape/autoscaling/RestErrorHandler.java
@@ -0,0 +1,18 @@
+package jp.ac.nii.prl.mape.autoscaling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import jp.ac.nii.prl.mape.autoscaling.controller.DeploymentNotFoundException;
+
+@ControllerAdvice
+public class RestErrorHandler {
+
+	@ExceptionHandler(DeploymentNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public void handleDeploymentNotFoundException(DeploymentNotFoundException ex) {
+		
+	}
+}
