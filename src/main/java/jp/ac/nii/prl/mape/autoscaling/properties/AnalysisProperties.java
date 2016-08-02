df815de7add8798c0a51f5ad05327412216a4ae7
Lionel Montrieux <lionel@nii.ac.jp>
2016-08-02 17:04:08 +0900

    Properties for analysis

---
+++ src/main/java/jp/ac/nii/prl/mape/autoscaling/properties/AnalysisProperties.java
@@ -0,0 +1,64 @@
+package jp.ac.nii.prl.mape.autoscaling.properties;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("app.analysis")
+public class AnalysisProperties {
+
+	/**
+	 * Max CPU utilization threshold, before scaling up is triggered
+	 */
+	@Valid
+	@NotNull
+	@Min(0)
+	@Max(1)
+	private Double maxThreshold;
+	
+	/**
+	 * Min CPU utilization threshold, before scaling down is triggered
+	 */
+	@Valid
+	@NotNull
+	@Min(0)
+	@Max(1)
+	private Double minThreshold;
+	
+	/**
+	 * Scaling up percentage
+	 */
+	@Valid
+	@NotNull
+	@Min(0)
+	private Double scaleUp;
+
+	public Double getMaxThreshold() {
+		return maxThreshold;
+	}
+
+	public Double getMinThreshold() {
+		return minThreshold;
+	}
+
+	public Double getScaleUp() {
+		return scaleUp;
+	}
+
+	public void setMaxThreshold(Double maxThreshold) {
+		this.maxThreshold = maxThreshold;
+	}
+
+	public void setMinThreshold(Double minThreshold) {
+		this.minThreshold = minThreshold;
+	}
+
+	public void setScaleUp(Double scaleUp) {
+		this.scaleUp = scaleUp;
+	}
+}
