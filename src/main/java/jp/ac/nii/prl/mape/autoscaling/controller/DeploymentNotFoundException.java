247dff9315cd5b62362a9a6c003632baf47f6ee0
Lionel Montrieux <lionel@montrieux.eu>
2017-05-22 21:45:52 +0200

    Cleanup

--- src/main/java/jp/ac/nii/prl/mape/autoscaling/controller/DeploymentNotFoundException.java
+++ src/main/java/jp/ac/nii/prl/mape/autoscaling/controller/DeploymentNotFoundException.java
@@ -4,9 +4,6 @@ import org.springframework.web.client.RestClientException;
 
 public class DeploymentNotFoundException extends RestClientException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public DeploymentNotFoundException(final String msg) {
